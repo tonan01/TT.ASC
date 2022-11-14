fcd1cc00e6600ccfb98e69f11fe92c653c82069d
DELL <DELL@DESKTOP-QGV07E1>
2022-11-14 07:51:01 +0700

    Add project files.

---
+++ TT.DoAn/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TT.DoAn.Models;
+using System.Web.Services.Description;
+using System.Data.Entity.Infrastructure;
+
+namespace TT.DoAn.Controllers
+{
+    public class HomeController : Controller
+    {
+        ThucTap_DoAnDataContext db = new ThucTap_DoAnDataContext();
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        #region Quản lý phiếu thu
+
+        public ActionResult GetDanhSachPhieuThu()
+        {
+            return View();
+        }
+        //json phiếu thu
+        public JsonResult Read_DanhSachPhieuThu([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(db.sp_DanhSachQuanLyThu().ToList().ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+        //json chi tiết phiếu thu
+        public ActionResult Read_DanhSachChiTietPhieuThu(string pSoPhieu,[DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(db.sp_ChiTietPhieuThu("1002").ToList().ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+        //Delete
+        public ActionResult DeletePhieuThu(string pSoPhieu, [DataSourceRequest] DataSourceRequest request)
+        {
+
+            if (pSoPhieu == null)
+            {
+                return HttpNotFound();
+            }
+
+            try
+            {
+                db.sp_DeletePhieuThu(pSoPhieu);
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return RedirectToAction("GetDanhSachPhieuThu", "Home");
+            #endregion
+        }
+    } 
+}
---
+++ TT.DoAn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TT.DoAn.MvcApplication" Language="C#" %>
